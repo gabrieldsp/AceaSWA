94338241a7978c5ef4616321c21fd97059d4b901
Gabriel P <gabriel.dspinheiro@gmail.com>
2021-01-17 17:07:25 -0300

    Just Correcting the Variable Name

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ import numpy as np
 Auser = pd.read_csv('Aquifer_Auser.csv')
 Auser.dropna(subset=['Depth_to_Groundwater_SAL'], inplace=True)
 y = Auser['Depth_to_Groundwater_SAL']
-X = Auser.drop('Depth_to_Groundwater', axis='column')
+X = Auser.drop('Depth_to_Groundwater_SAL', axis=1)
